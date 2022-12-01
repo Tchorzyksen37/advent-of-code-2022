23eebcf4afa878ae90b3d62950851481a873db92
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2022-12-01 11:57:57 +0100

    Finish day 1 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
+import java.util.stream.Collectors
+
 fun main() {
+    fun getTotalCaloriesCarriedList(input: List<String>): List<Int> {
+        var caloriesSum = 0
+        val caloriesSums = mutableListOf<Int>()
+        for (str in input) {
+            if (str.isBlank()) {
+                caloriesSums.add(caloriesSum)
+                caloriesSum = 0
+            } else {
+                caloriesSum += str.toInt()
+            }
+        }
+        if (caloriesSum != 0) {
+            caloriesSums.add(caloriesSum)
+        }
+
+        return caloriesSums.stream().sorted().collect(Collectors.toList())
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val totalCaloriesCarriedList = getTotalCaloriesCarriedList(input)
+        return totalCaloriesCarriedList.last()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val totalCaloriesCarriedList = getTotalCaloriesCarriedList(input)
+        val totalCaloriesCarriedListSize = totalCaloriesCarriedList.size
+        return totalCaloriesCarriedList[totalCaloriesCarriedListSize - 1] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 2] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 3]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
