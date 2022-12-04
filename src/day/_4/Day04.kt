134c0b026258ae83369bfd020e29ead191660f0d
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2022-12-04 22:01:40 +0100

    :sparkles: Solve Day 4 Challenge
    Solve day 4 challenge part 1 and 2

--- src/day/_4/Day04.kt
+++ src/day/_4/Day04.kt
@@ -1,24 +1,58 @@
 package day._4
 
 import readInput
-import java.util.stream.Collectors
 
 fun main() {
 
-fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		var overlappingSectionsCount = 0
+		for (str in input) {
+			println(str)
+			val elvesPair = str.split(",")
+			val sectionOneRange = elvesPair[0].split("-")
+			val sectionTwoRange = elvesPair[1].split("-")
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+			if ((Integer.parseInt(sectionOneRange[0]) >= Integer.parseInt(sectionTwoRange[0])
+							&& Integer.parseInt(sectionOneRange[1]) <= Integer.parseInt(sectionTwoRange[1]))
+					|| (Integer.parseInt(sectionTwoRange[0]) >= Integer.parseInt(sectionOneRange[0])
+							&& Integer.parseInt(sectionTwoRange[1]) <= Integer.parseInt(sectionOneRange[1]))) {
+				overlappingSectionsCount++
+			}
 
-    val testInput = readInput("day/_4/Day04_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+		}
+		return overlappingSectionsCount
+	}
 
-    val input = readInput("day/_4/Day04")
-    println(part1(input))
-    println(part2(input))
+	fun part2(input: List<String>): Int {
+		var overlappingSectionsCount = 0
+		for (str in input) {
+			println(str)
+			val elvesPair = str.split(",")
+			val sectionOneRange = elvesPair[0].split("-")
+			val sectionTwoRange = elvesPair[1].split("-")
+
+			if ((Integer.parseInt(sectionOneRange[0]) >= Integer.parseInt(sectionTwoRange[0])
+							&& Integer.parseInt(sectionOneRange[0]) <= Integer.parseInt(sectionTwoRange[1]))
+					|| (Integer.parseInt(sectionOneRange[1]) <= Integer.parseInt(sectionTwoRange[1])
+							&& Integer.parseInt(sectionOneRange[1]) >= Integer.parseInt(sectionTwoRange[0]))
+					|| (Integer.parseInt(sectionTwoRange[0]) >= Integer.parseInt(sectionOneRange[0])
+							&& Integer.parseInt(sectionTwoRange[0]) <= Integer.parseInt(sectionOneRange[1]))
+					|| (Integer.parseInt(sectionTwoRange[1]) <= Integer.parseInt(sectionOneRange[1])
+							&& Integer.parseInt(sectionTwoRange[1]) >= Integer.parseInt(sectionOneRange[0]))) {
+				overlappingSectionsCount++
+				println("Sections are overlapping. Count $overlappingSectionsCount")
+			}
+
+		}
+		return overlappingSectionsCount
+	}
+
+	val testInput = readInput("day/_4/Day04_test")
+	check(part1(testInput) == 2)
+	check(part2(testInput) == 4)
+
+	val input = readInput("day/_4/Day04")
+	println(part1(input))
+	println(part2(input))
 
 }
