42816d7def56438331e705838970e02c542883ae
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2022-12-05 00:00:36 +0100

    :art: Improve Day 1 Challenge Solution
    Make use of Kotlin features

--- src/day/_1/Day01.kt
+++ src/day/_1/Day01.kt
@@ -1,43 +1,21 @@
 package day._1
 
 import readInput
-import java.util.stream.Collectors
 
 fun main() {
-	fun getTotalCaloriesCarriedSortedList(input: List<String>): List<Int> {
-		var caloriesSum = 0
-		val caloriesSums = mutableListOf<Int>()
-		for (str in input) {
-			if (str.isBlank()) {
-				caloriesSums.add(caloriesSum)
-				caloriesSum = 0
-			} else {
-				caloriesSum += str.toInt()
-			}
-		}
-		if (caloriesSum != 0) {
-			caloriesSums.add(caloriesSum)
-		}
 
-		return caloriesSums.stream().sorted().collect(Collectors.toList())
-	}
+	fun input(filePath: String): List<Int> = readInput(filePath).joinToString("\n") { it.trim() }.split("\n\n")
+		.map { block -> block.lines().mapNotNull { if (it.trim().isEmpty()) null else it.trim().toInt() } }.map { it.sum() }
 
-	fun part1(input: List<String>): Int {
-		val totalCaloriesCarriedList = getTotalCaloriesCarriedSortedList(input)
-		return totalCaloriesCarriedList.last()
-	}
+	fun part1(input: List<Int>): Int = input.max()
 
-	fun part2(input: List<String>): Int {
-		val totalCaloriesCarriedList = getTotalCaloriesCarriedSortedList(input)
-		val totalCaloriesCarriedListSize = totalCaloriesCarriedList.size
-		return totalCaloriesCarriedList[totalCaloriesCarriedListSize - 1] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 2] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 3]
-	}
+	fun part2(input: List<Int>): Int = input.sortedDescending().take(3).sum()
 
-	val testInput = readInput("day/_1/Day01_test")
+	val testInput = input("day/_1/Day01_test")
 	check(part1(testInput) == 24000)
 	check(part2(testInput) == 45000)
 
-	val input = readInput("day/_1/Day01")
+	val input = input("day/_1/Day01")
 	println(part1(input))
 	println(part2(input))
 
