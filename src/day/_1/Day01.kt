933262efcffee4a803b21ce5636129ce7a4c06eb
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2022-12-04 22:04:09 +0100

    :art: Use custom Kotlin code style formatter
    Use for code indent
    - smart tabs
    - tabs instead of spaces
    - change size from 4 - 2 (Spaces)

--- src/day/_1/Day01.kt
+++ src/day/_1/Day01.kt
@@ -4,41 +4,41 @@ import readInput
 import java.util.stream.Collectors
 
 fun main() {
-    fun getTotalCaloriesCarriedSortedList(input: List<String>): List<Int> {
-        var caloriesSum = 0
-        val caloriesSums = mutableListOf<Int>()
-        for (str in input) {
-            if (str.isBlank()) {
-                caloriesSums.add(caloriesSum)
-                caloriesSum = 0
-            } else {
-                caloriesSum += str.toInt()
-            }
-        }
-        if (caloriesSum != 0) {
-            caloriesSums.add(caloriesSum)
-        }
+	fun getTotalCaloriesCarriedSortedList(input: List<String>): List<Int> {
+		var caloriesSum = 0
+		val caloriesSums = mutableListOf<Int>()
+		for (str in input) {
+			if (str.isBlank()) {
+				caloriesSums.add(caloriesSum)
+				caloriesSum = 0
+			} else {
+				caloriesSum += str.toInt()
+			}
+		}
+		if (caloriesSum != 0) {
+			caloriesSums.add(caloriesSum)
+		}
 
-        return caloriesSums.stream().sorted().collect(Collectors.toList())
-    }
+		return caloriesSums.stream().sorted().collect(Collectors.toList())
+	}
 
-    fun part1(input: List<String>): Int {
-        val totalCaloriesCarriedList = getTotalCaloriesCarriedSortedList(input)
-        return totalCaloriesCarriedList.last()
-    }
+	fun part1(input: List<String>): Int {
+		val totalCaloriesCarriedList = getTotalCaloriesCarriedSortedList(input)
+		return totalCaloriesCarriedList.last()
+	}
 
-    fun part2(input: List<String>): Int {
-        val totalCaloriesCarriedList = getTotalCaloriesCarriedSortedList(input)
-        val totalCaloriesCarriedListSize = totalCaloriesCarriedList.size
-        return totalCaloriesCarriedList[totalCaloriesCarriedListSize - 1] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 2] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 3]
-    }
+	fun part2(input: List<String>): Int {
+		val totalCaloriesCarriedList = getTotalCaloriesCarriedSortedList(input)
+		val totalCaloriesCarriedListSize = totalCaloriesCarriedList.size
+		return totalCaloriesCarriedList[totalCaloriesCarriedListSize - 1] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 2] + totalCaloriesCarriedList[totalCaloriesCarriedListSize - 3]
+	}
 
-    val testInput = readInput("day/_1/Day01_test")
-    check(part1(testInput) == 24000)
-    check(part2(testInput) == 45000)
+	val testInput = readInput("day/_1/Day01_test")
+	check(part1(testInput) == 24000)
+	check(part2(testInput) == 45000)
 
-    val input = readInput("day/_1/Day01")
-    println(part1(input))
-    println(part2(input))
+	val input = readInput("day/_1/Day01")
+	println(part1(input))
+	println(part2(input))
 
 }
--- src/day/_3/Day03.kt
+++ src/day/_3/Day03.kt
@@ -5,67 +5,67 @@ import java.util.stream.Collectors
 
 fun main() {
 
-    fun stringToCharacterList(str: String): List<String> {
-        return str.chars().mapToObj(Character::toString).collect(Collectors.toList())
-    }
+	fun stringToCharacterList(str: String): List<String> {
+		return str.chars().mapToObj(Character::toString).collect(Collectors.toList())
+	}
 
-    fun mapElementToIntValue(element: String): Int {
-        return if (element[0].isLowerCase()) (element.codePointAt(0) - 96) else (element.codePointAt(0) - 38)
+	fun mapElementToIntValue(element: String): Int {
+		return if (element[0].isLowerCase()) (element.codePointAt(0) - 96) else (element.codePointAt(0) - 38)
 
-    }
+	}
 
-    fun part1(input: List<String>): Int {
-        var theSumOfPriorities = 0
-        for (str in input) {
-            val firstElementCompartmentOne = stringToCharacterList(str.subSequence(0, str.length / 2).toString())
-            val firstElementCompartmentTwo = stringToCharacterList(str.subSequence(str.length / 2, str.length).toString())
-            val duplicate = firstElementCompartmentOne.stream().filter { ch -> firstElementCompartmentTwo.contains(ch) }.findFirst()
-            if (duplicate.isPresent) {
-                theSumOfPriorities += mapElementToIntValue(duplicate.get())
-            }
-        }
+	fun part1(input: List<String>): Int {
+		var theSumOfPriorities = 0
+		for (str in input) {
+			val firstElementCompartmentOne = stringToCharacterList(str.subSequence(0, str.length / 2).toString())
+			val firstElementCompartmentTwo = stringToCharacterList(str.subSequence(str.length / 2, str.length).toString())
+			val duplicate = firstElementCompartmentOne.stream().filter { ch -> firstElementCompartmentTwo.contains(ch) }.findFirst()
+			if (duplicate.isPresent) {
+				theSumOfPriorities += mapElementToIntValue(duplicate.get())
+			}
+		}
 
-        return theSumOfPriorities
-    }
+		return theSumOfPriorities
+	}
 
-    fun mapToGroups(input: List<String>): List<List<String>> {
-        val groups = mutableListOf<List<String>>()
-        var group = mutableListOf<String>()
-        for ((index, value) in input.withIndex()) {
-            group.add(value)
+	fun mapToGroups(input: List<String>): List<List<String>> {
+		val groups = mutableListOf<List<String>>()
+		var group = mutableListOf<String>()
+		for ((index, value) in input.withIndex()) {
+			group.add(value)
 
-            if (index % 3 == 2) {
-                groups.add(group)
-                group = mutableListOf()
-            }
-        }
+			if (index % 3 == 2) {
+				groups.add(group)
+				group = mutableListOf()
+			}
+		}
 
-        return groups
-    }
+		return groups
+	}
 
-    fun part2(input: List<String>): Int {
-        val groups = mapToGroups(input)
-        var badgesSum = 0
-        for (group in groups) {
-            val firstCrewMember = stringToCharacterList(group[0])
-            val secondCrewMember = stringToCharacterList(group[1])
-            val thirdCrewMember = stringToCharacterList(group[2])
+	fun part2(input: List<String>): Int {
+		val groups = mapToGroups(input)
+		var badgesSum = 0
+		for (group in groups) {
+			val firstCrewMember = stringToCharacterList(group[0])
+			val secondCrewMember = stringToCharacterList(group[1])
+			val thirdCrewMember = stringToCharacterList(group[2])
 
-            val duplicate = firstCrewMember.stream().filter { ch -> secondCrewMember.contains(ch) }.filter { ch -> thirdCrewMember.contains(ch) }.findFirst()
-            if (duplicate.isPresent) {
-                badgesSum += mapElementToIntValue(duplicate.get())
-            }
+			val duplicate = firstCrewMember.stream().filter { ch -> secondCrewMember.contains(ch) }.filter { ch -> thirdCrewMember.contains(ch) }.findFirst()
+			if (duplicate.isPresent) {
+				badgesSum += mapElementToIntValue(duplicate.get())
+			}
 
-        }
-        return badgesSum
-    }
+		}
+		return badgesSum
+	}
 
-    val testInput = readInput("day/_3/Day03_test")
-    check(part1(testInput) == 157)
-    check(part2(testInput) == 70)
+	val testInput = readInput("day/_3/Day03_test")
+	check(part1(testInput) == 157)
+	check(part2(testInput) == 70)
 
-    val input = readInput("day/_3/Day03")
-    println(part1(input))
-    println(part2(input))
+	val input = readInput("day/_3/Day03")
+	println(part1(input))
+	println(part2(input))
 
 }
