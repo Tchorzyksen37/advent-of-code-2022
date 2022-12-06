56a1ddf04c5bf2c46d172a9a4415d48fd7bc02ee
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2022-12-06 15:28:58 +0100

    Solve Day 6 Challenge
    Solve day 6 challenge part 1 and 2

--- src/day/_6/Day06.kt
+++ src/day/_6/Day06.kt
@@ -4,17 +4,29 @@ import readInput
 
 fun main() {
 
+	fun lookUpStartMarker(signal: String, startMarkerLength: Int): Int {
+		for (idx in startMarkerLength..signal.length) {
+			val subStringAsSet = signal.substring(startIndex = idx - startMarkerLength, endIndex = idx)
+				.split("")
+				.mapNotNull { it.ifBlank { null } }
+				.toSet()
+			if (subStringAsSet.size < startMarkerLength) continue
+			return idx
+		}
+		return -1
+	}
+
 	fun part1(input: List<String>): Int {
-		return input.size
+		return lookUpStartMarker(input.first(), 4)
 	}
 
 	fun part2(input: List<String>): Int {
-		return input.size
+		return lookUpStartMarker(input.first(), 14)
 	}
 
 	val testInput = readInput("day/_6/Day06_test")
-	check(part1(testInput) == 0)
-	check(part2(testInput) == 0)
+	check(part1(testInput) == 5)
+	check(part2(testInput) == 23)
 
 	val input = readInput("day/_6/Day06")
 	println(part1(input))
